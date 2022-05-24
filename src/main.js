5f9efc111abf6c3e9caac27024bbc59a7884c815
DaniPoot <danielpoot.uc2@gmail.com>
2022-05-23 19:54:19 -0500

    Add to vue the store

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import Argon from './plugins/argon-kit'
+import store from './store'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
@@ -9,5 +10,6 @@ Vue.config.productionTip = false
 Vue.use(Argon)
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
