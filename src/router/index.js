268be1e50db87433c022600416090abe3cbe34f3
DaniPoot <danielpoot.uc2@gmail.com>
2022-05-29 14:13:50 -0500

    Modify router index to use the store inside

--- src/router/index.js
+++ src/router/index.js
@@ -4,16 +4,24 @@ import routes from './routes'
 
 Vue.use(Router)
 
-const router = new Router({
-  linkExactActiveClass: 'active',
-  routes,
-  scrollBehavior: to => {
-    if (to.hash) {
-      return { selector: to.hash }
-    } else {
-      return { x: 0, y: 0 }
+export default function ({ store }) {
+  const router = new Router({
+    linkExactActiveClass: 'active',
+    routes,
+    scrollBehavior: to => {
+      if (to.hash) {
+        return { selector: to.hash }
+      } else {
+        return { x: 0, y: 0 }
+      }
     }
-  }
-})
+  })
 
-export default router
+  router.beforeEach((to, from, next) => {
+    const isAuthenticated = store.getters['accounts/isAuthenticated']
+    if (to.name !== 'login' && !isAuthenticated) next({ name: 'login' })
+    else next()
+  })
+
+  return router
+}
