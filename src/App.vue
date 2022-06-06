301dada79aad675a23ee233977b4b20bf40c22e5
DaniPoot <danielpoot.uc2@gmail.com>
2022-06-06 18:51:40 -0500

    Add a main loader

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,25 @@
                 <router-view/>
             </fade-transition>
         </main>
+        <modal :show.sync="isLoading" modal-classes="modal-dialog-centered" modalContentClasses="bg-transparent shadow-none text-center" type="mini">
+          <loader />
+        </modal>
     </div>
 </template>
 <script>
+import { mapGetters } from 'vuex'
 import { FadeTransition } from 'vue2-transitions'
+import Modal from '@/components/Modal.vue'
+import Loader from '@/components/Loader'
 
 export default {
   components: {
-    FadeTransition
+    FadeTransition,
+    Modal,
+    Loader
+  },
+  computed: {
+    ...mapGetters('general', ['isLoading'])
   }
 }
 </script>
